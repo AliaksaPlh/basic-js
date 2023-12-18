a0d75791e15516a802545994372e0e8570f14604
Aliaksa <sashvero@gmail.com>
2023-12-18 14:39:16 +0300

    fix: ciunt cat

--- src/count-cats.js
+++ src/count-cats.js
@@ -17,11 +17,6 @@ const { NotImplementedError } = require("../extensions/index.js");
 function countCats(matrix) {
   let sum = 0;
   for (let i = 0; i < matrix.length; i++) {
-    // for (let j = 0; j < matrix[i].length; j++) {
-    //   if (matrix[i][j] == "^^") {
-    //     sum = sum + 1;
-    //   }
-    // }
     for (let item of matrix[i]) {
       if (item == "^^") {
         sum = sum + 1;
